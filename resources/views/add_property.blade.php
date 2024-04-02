6686e25dc82cbc66597454be58874a8be827f465
Chanaka Eshan <chanakaeshaninfo@gmail.com>
2024-04-02 19:29:17 +0530

    Add Google Maps functionality to add_property view

--- resources/views/add_property.blade.php
+++ resources/views/add_property.blade.php
@@ -5,6 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Property</title>
     <link rel="stylesheet" href="css/landlord.css">
+    <style>
+        /* CSS styles for map container */
+        #google-map {
+            height: 400px;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
     <div class="container">
@@ -18,8 +25,46 @@
             <input type="file" id="image" name="image" accept="image/*" required>
             <label for="price">Price:</label>
             <input type="number" id="price" name="price" min="0" required>
+            <div id="google-map"></div>
+
+    <script>
+        function initMap() {
+            var map = new google.maps.Map(document.getElementById('google-map'), {
+                center: { lat: 6.824265618550028, lng: 80.04001213226317 },
+                zoom: 15
+            });
+
+            var marker;
+
+            // Add click event listener to the map
+            map.addListener('click', function(event) {
+                placeMarker(event.latLng);
+            });
+
+            // Function to place marker at the clicked location
+            function placeMarker(location) {
+                if (marker) {
+                    marker.setPosition(location);
+                } else {
+                    marker = new google.maps.Marker({
+                        position: location,
+                        map: map
+                    });
+                }
+                // Set latitude and longitude values to hidden inputs
+                document.getElementById('lat').value = location.lat();
+                document.getElementById('lng').value = location.lng();
+            }
+        }
+    </script>
+
+    <!-- Load Google Maps API with callback to initMap function -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script>
             <button type="submit">Add Property</button>
         </form>
     </div>
+
+    <!-- Google Maps API -->
+    
 </body>
 </html>
